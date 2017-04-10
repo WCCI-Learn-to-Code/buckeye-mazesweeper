69dd4af5191e6a05ecf147d60165817da9ad9044
B T Forsythe <brian@btforsythe.io>
2017-04-10 15:55:01 -0400

    making progress

--- build.sh
+++ build.sh
@@ -1,1 +1,27 @@
-slideshow build -t slidy index.md -o docs
+rm -rf docs
+
+mkdir docs
+cat headers.txt index.md styleTweaks.html > docs/index.md
+
+# reveal.js template
+# author: Your Name Here
+# title:  Your Slide Show Title Here
+# theme:  black|white|league|sky|beige|simple|serif|blood|night|moon|solarized
+slideshow build -t reveal.js docs/index.md -o docs
+
+# slidy template
+# slideshow build -t slidy index.md -o docs
+
+# shower template
+# author: Your Name Here
+# title: Your Slide Show Title Here
+# cover: Your Slide Show Cover Image Here
+# lang: Your Language Here
+# theme:  ribbon|material
+# slideshow build -t shower index.md -o docs
+
+mkdir docs/images
+cp -r images/* docs/images
+
+mkdir docs/examples
+cp -r examples/* docs/examples
