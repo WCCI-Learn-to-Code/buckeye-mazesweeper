c99b57f0263a284d87c5fcefdd4d1cc58df78a87
B T Forsythe <brian@btforsythe.io>
2017-04-16 15:52:57 -0400

    refactoring: eval for operation

--- examples/calculator/js/calc.js
+++ examples/calculator/js/calc.js
@@ -1,34 +1,23 @@
 function create() {
 	var displayed = 0;
 
-	var operationsByOperator = {
-		"+": function(lval, rval) { displayed = lval + rval },
-		"-": function(lval, rval) { displayed = lval - rval },
-		"*": function() { displayed = 24 },
-		"/": function() { displayed = 4 }
-	}
-
 	var operationPending = false;
 	var operation;
 	var lval;
-
-	function displayedAsNumber() {
-		return parseFloat(displayed);
-	}
 
 	return {
 		display: function() { return "" + displayed; },
 		press: function(pressed) {
 
 			if(pressed == "=") {
-				operation(lval, displayedAsNumber());
+				operation(lval, displayed);
 				return;
 			}
 
 			if(isNaN(pressed)) {
-				lval = displayedAsNumber();
+				lval = displayed;
 				operationPending = true;
-				operation = operationsByOperator[pressed];
+				operation = operationFor(pressed);
 				return;
 			}
 			
@@ -38,6 +27,10 @@ function create() {
 			displayed += pressed;
 		}
 	};
+
+	function operationFor(operator) {
+		return (lval, rval) => ( displayed = eval("" + lval + operator + rval));
+	}
 }
 
 module.exports = create;
