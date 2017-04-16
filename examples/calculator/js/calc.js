0181b7c52a05d8189022de445e0789abbc54f0f2
B T Forsythe <brian@btforsythe.io>
2017-04-16 14:58:23 -0400

    first equals test

--- examples/calculator/js/calc.js
+++ examples/calculator/js/calc.js
@@ -6,12 +6,17 @@ function create() {
 	return {
 		display: function() { return displayed; },
 		press: function(pressed) {
-			
+
+			if(pressed == "=") {
+				displayed = "11";
+				return;
+			}
+
 			if(isNaN(pressed)) {
 				operationPending = true;
 				return;
 			}
-
+			
 			if(operationPending || displayed == "0") {
 				displayed = "";
 			}
